275029d879e8813438139042ff2bd4f29ce01c17
ArcherCBC <a111223013@mail.shu.edu.tw>
2025-09-09 13:33:54 +0800

    update:authService

--- src/app/service/overview.service.ts
+++ src/app/service/overview.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CalendarEvent } from 'angular-calendar';
+import { AuthService } from './auth.service';
 
 export interface CalendarEventResponse {
   events: any[];
@@ -30,7 +31,7 @@ export interface EventResponse {
 export class OverviewService {
   private baseUrl = 'http://localhost:5000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   private getHeaders(): HttpHeaders {
     const token = localStorage.getItem('token');
@@ -41,26 +42,34 @@ export class OverviewService {
   }
 
   getCalendarEvents(): Observable<any> {
-    return this.http.post<CalendarEventResponse>(`${this.baseUrl}/dashboard/events`, {}, {
-      headers: this.getHeaders()
-    });
+    return this.authService.authenticatedRequest(() => 
+      this.http.post<CalendarEventResponse>(`${this.baseUrl}/dashboard/events`, {}, {
+        headers: this.getHeaders()
+      })
+    );
   }
 
   createCalendarEvent(eventData: CreateEventRequest): Observable<any> {
-    return this.http.post<EventResponse>(`${this.baseUrl}/dashboard/events/create`, eventData, {
-      headers: this.getHeaders()
-    });
+    return this.authService.authenticatedRequest(() => 
+      this.http.post<EventResponse>(`${this.baseUrl}/dashboard/events/create`, eventData, {
+        headers: this.getHeaders()
+      })
+    );
   }
 
   updateCalendarEvent(eventId: number, eventData: CreateEventRequest): Observable<any> {
-    return this.http.post<EventResponse>(`${this.baseUrl}/dashboard/events/update`, { ...eventData, event_id: eventId }, {
-      headers: this.getHeaders()
-    });
+    return this.authService.authenticatedRequest(() => 
+      this.http.post<EventResponse>(`${this.baseUrl}/dashboard/events/update`, { ...eventData, event_id: eventId }, {
+        headers: this.getHeaders()
+      })
+    );
   }
 
   deleteCalendarEvent(eventId: number): Observable<any> {
-    return this.http.post<EventResponse>(`${this.baseUrl}/dashboard/events/delete`, { event_id: eventId }, {
-      headers: this.getHeaders()
-    });
+    return this.authService.authenticatedRequest(() => 
+      this.http.post<EventResponse>(`${this.baseUrl}/dashboard/events/delete`, { event_id: eventId }, {
+        headers: this.getHeaders()
+      })
+    );
   }
 }
