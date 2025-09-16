e6a3cf49304ffb8dac779b9a8d169f31a6fb1908
a111223007 <a111223007@mail.shu.edu.tw>
2025-09-17 03:35:04 +0800

    feat: 註冊 Swiper 自定義元素

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
@@ -50,6 +51,9 @@ import {
   cilSettings,
   cilMediaPlay
 } from '@coreui/icons';
+import { register as registerSwiperElements } from 'swiper/element/bundle';
+
+registerSwiperElements();
 
 bootstrapApplication(AppComponent, {
   providers: [
