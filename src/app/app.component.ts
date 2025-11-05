8c0d2dec262ba3f9fb196daa5c096d3547bd2b52
ArcherCBC <a111223013@mail.shu.edu.tw>
2025-11-05 12:10:46 +0800

    logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-root',
@@ -8,6 +9,13 @@ import { RouterOutlet } from '@angular/router';
     standalone: true,
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MIS_Teach';
+
+  constructor(private titleService: Title) {}
+
+  ngOnInit() {
+    // 設定瀏覽器標題
+    this.titleService.setTitle('MIS_Teach - 智慧學習系統');
+  }
 }
